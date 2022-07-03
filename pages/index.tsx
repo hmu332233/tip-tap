61f51e4b9b13ed5b4697061d44c760785437ed60
Minung Han <hmu332233@gmail.com>
2022-07-03 18:02:54 +0900

    canvas 기반 동작에서 element 기반 동작으로 변경

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,11 @@
 // import DrawRound from 'components/DrawRound';
+import DrawRound from 'components/DrawRound';
 import type { NextPage } from 'next';
 // import Head from 'next/head';
 // import Image from 'next/image';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
-function log(msg: any) {
-  const p = document.getElementById('log');
-  if (!p) {
-    return;
-  }
-
-  p.innerHTML = msg + '\n' + p.innerHTML;
-}
-
-function drawRound(ctx: any, touch: any) {
-  ctx?.clearRect(0, 0, 600, 600);
-  // ctx.fillRect(touch.pageX - 20, touch.pageY - 20, 40,40); // and a square at the end
-
-  ctx.arc(touch.pageX, touch.pageY, 40, 0, 2 * Math.PI, false); // a circle at the start
-  ctx.fill();
-}
+const log = console.log;
 
 function copyTouch(touch: any) {
   const color = colorForTouch(touch);
@@ -40,136 +26,75 @@ function colorForTouch(touch: any) {
   return color;
 }
 
-var ongoingTouches: any = {};
+const useTouches = () => {
+  const [ongoingTouches, setOngoingTouches] = useState({});
 
-function handleStart(evt: any) {
-  evt.preventDefault();
-  log('touchstart.');
-  var el = document.getElementsByTagName('canvas')[0];
-  var ctx = el.getContext('2d');
-  var touches = evt.changedTouches;
+  useEffect(() => {
+    function handleStart(event: TouchEvent) {
+      event.preventDefault();
+      log('touchstart.');
+      // event.changedTouches.length;
 
-  if (!ctx) {
-    return;
-  }
+      const touchesObj: { [key: string]: any } = {};
+      for (let i = 0; i < event.changedTouches.length; i++) {
+        const touch = copyTouch(event.changedTouches[i]);
+        touchesObj[touch.identifier] = touch;
+      }
 
-  for (var i = 0; i < touches.length; i++) {
-    log('touchstart:' + i + '...');
-    const touch = copyTouch(touches[i]);
-    ongoingTouches[touch.identifier] = touch;
-
-    ctx.beginPath();
-    // ctx.arc(touch.pageX, touch.pageY, 4, 0, 2 * Math.PI, false); // a circle at the start
-    // ctx.fillStyle = touch.color;
-    // ctx.fill();
-    drawRound(ctx, touch);
-    log('touchstart:' + i + '.');
-  }
-}
-
-function startup() {
-  var el = document.getElementsByTagName('canvas')[0];
-  el.addEventListener('touchstart', handleStart, false);
-  el.addEventListener('touchend', handleEnd, false);
-  el.addEventListener('touchcancel', handleCancel, false);
-  el.addEventListener('touchmove', handleMove, false);
-  log('initialized.');
-}
-
-function handleMove(evt: any) {
-  evt.preventDefault();
-  var el = document.getElementsByTagName('canvas')[0];
-  var ctx = el.getContext('2d');
-  var touches = evt.changedTouches;
-
-  if (!ctx) {
-    return;
-  }
-
-  for (var i = 0; i < touches.length; i++) {
-    // var color = colorForTouch(touches[i]);
-    // var idx = ongoingTouchIndexById(touches[i].identifier);
-
-    const touch = ongoingTouches[touches[i].identifier];
-    const newTouch = copyTouch(touches[i]);
-
-    if (touch) {
-      log('continuing touch ' + touch.identifier);
-      ctx.beginPath();
-      log('ctx.moveTo(' + newTouch.pageX + ', ' + newTouch.pageY + ');');
-      // ctx.moveTo(touch.pageX, touch.pageY);
-      // log('ctx.lineTo(' + touch.pageX + ', ' + touch.pageY + ');');
-      // ctx.lineTo(newTouch.pageX, newTouch.pageY);
-      // ctx.lineWidth = 40;
-      // ctx.strokeStyle = newTouch.color;
-      // ctx.stroke();
-
-      drawRound(ctx, newTouch);
-      ongoingTouches[newTouch.identifier] = newTouch;
-      log('.');
-    } else {
-      log("can't figure out which touch to continue");
+      setOngoingTouches((arr) => ({ ...arr, ...touchesObj }));
     }
-  }
-}
 
-function handleEnd(evt: any) {
-  evt.preventDefault();
-  log('touchend');
-  var el = document.getElementsByTagName('canvas')[0];
-  var ctx = el.getContext('2d');
-  var touches = evt.changedTouches;
+    function handleMove(event: TouchEvent) {
+      event.preventDefault();
 
-  if (!ctx) {
-    return;
-  }
+      const touchesObj: { [key: string]: any } = {};
+      for (let i = 0; i < event.changedTouches.length; i++) {
+        const touch = copyTouch(event.changedTouches[i]);
+        touchesObj[touch.identifier] = touch;
+      }
 
-  for (var i = 0; i < touches.length; i++) {
-    // var color = colorForTouch(touches[i]);
-    // var idx = ongoingTouchIndexById(touches[i].identifier);
-    const touch = ongoingTouches[touches[i].identifier];
-    const newTouch = copyTouch(touches[i]);
-
-    if (touch) {
-      ctx.lineWidth = 40;
-      ctx.fillStyle = touch.color;
-      ctx.beginPath();
-      // ctx.moveTo(touch.pageX, touch.pageY);
-      // ctx.lineTo(newTouch.pageX, newTouch.pageY);
-
-      drawRound(ctx, newTouch);
-      ongoingTouches[touch.identifier] = null;
-    } else {
-      log("can't figure out which touch to end");
+      setOngoingTouches((arr) => ({ ...arr, ...touchesObj }));
     }
-  }
-}
 
-function handleCancel(evt: any) {
-  evt.preventDefault();
-  log('touchcancel.');
-  var touches = evt.changedTouches;
+    function handleEnd(event: TouchEvent) {
+      event.preventDefault();
+      log('touchend');
 
-  for (var i = 0; i < touches.length; i++) {
-    ongoingTouches[touches[i].identifier] = null;
-  }
-}
+      const touchesObj: { [key: string]: any } = {};
+      for (let i = 0; i < event.changedTouches.length; i++) {
+        const touch = copyTouch(event.changedTouches[i]);
+        touchesObj[touch.identifier] = undefined;
+      }
+
+      setOngoingTouches((arr) => ({ ...arr, ...touchesObj }));
+    }
+
+    document.addEventListener('touchstart', handleStart, false);
+    document.addEventListener('touchend', handleEnd, false);
+    document.addEventListener('touchcancel', handleEnd, false);
+    document.addEventListener('touchmove', handleMove, false);
+
+    return () => {
+      document.removeEventListener('touchstart', handleStart);
+      document.removeEventListener('touchend', handleEnd);
+      document.removeEventListener('touchcancel', handleEnd);
+      document.removeEventListener('touchmove', handleMove);
+    };
+  }, []);
+
+  return ongoingTouches;
+};
 
 const Home: NextPage = () => {
-  useEffect(() => {
-    startup();
-  }, []);
+  const ongoingTouches = useTouches();
+  console.log(ongoingTouches);
   return (
-    <div>
-      <canvas
-        id="canvas"
-        width="600"
-        height="600"
-        style={{ border: 'solid black 1px' }}
-      >
-        Your browser does not support canvas element.
-      </canvas>
-      Log: <pre id="log" style={{ border: '1px solid #ccc' }}></pre>
+    <div className="relative w-screen h-screen">
+      {Object.values(ongoingTouches)
+        .filter(Boolean)
+        .map((touch) => (
+          <DrawRound key={touch.identifier} x={touch.pageX} y={touch.pageY} />
+        ))}
     </div>
   );
 };
