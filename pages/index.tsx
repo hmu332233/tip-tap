1457a4dcc5d2ee6a67f6ce16924c116a752cc065
Minung Han <hmu332233@gmail.com>
2022-07-04 01:40:58 +0900

    화면 오른쪽으로 터치하면 화면이 흔들리던 버그 수정

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,9 @@
-// import DrawRound from 'components/DrawRound';
-import DrawRound from 'components/DrawRound';
 import type { NextPage } from 'next';
-// import Head from 'next/head';
-// import Image from 'next/image';
 import { useEffect, useState } from 'react';
 import { IOngoingTouches, ITouch } from 'types';
-import { sampleSize } from 'utils';
 
 import _sample from 'lodash/sample';
+import DrawRound from 'components/DrawRound';
 
 const log = process.env.NODE_ENV === 'development' ? console.log : () => {};
 
@@ -25,12 +21,17 @@ function colorForTouch(touch: ITouch) {
   const colorMap = [
     'primary',
     'secondary',
+    'secondary-content',
     'accent',
-    'neutral',
+    'neutral-content',
     'info',
+    'info-content',
+    'base-content',
     'success',
+    'success-content',
     'warning',
     'error',
+    'error-content',
   ];
   const index = touch.identifier % colorMap.length;
   return colorMap[index];
@@ -102,6 +103,7 @@ const Home: NextPage = () => {
     log(ongoingTouches);
     const touches = Object.values(ongoingTouches).filter(Boolean);
     log(touches);
+
     if (touches.length === 0) {
       setSelecting(false);
       return;
@@ -129,6 +131,7 @@ const Home: NextPage = () => {
       clearTimeout(id);
     };
   }, [selecting]);
+
   return (
     <div className="flex">
       {Object.values(ongoingTouches)
