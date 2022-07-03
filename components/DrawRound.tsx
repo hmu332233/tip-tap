d95eb9cc72ea91ddcca48ee7dfc0b536c2179cba
Minung Han <hmu332233@gmail.com>
2022-07-03 17:52:31 +0900

    DrawRound 컴포넌트 UI 수정

--- components/DrawRound.tsx
+++ components/DrawRound.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
 
-type Props = {};
+type Props = {
+  x: number;
+  y: number;
+};
 
-function DrawRound({}: Props) {
-  return <div className="rounded-full w-20 h-20 bg-primary" />;
+function DrawRound({ x, y }: Props) {
+  return (
+    <div
+      className="absolute -translate-x-1/2 -translate-y-1/2"
+      style={{ left: x, top: y }}
+    >
+      <div className="rounded-full w-20 h-20 border-8 border-dashed animate-spin" />
+    </div>
+  );
 }
 
 export default DrawRound;
