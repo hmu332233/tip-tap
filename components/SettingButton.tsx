97e3c35c4671dfaa324411cf6f9ff739cdcea45d
Minung Han <hmu332233@gmail.com>
2022-07-23 20:03:55 +0900

    Setting Icon 크기 변경

--- components/SettingButton.tsx
+++ components/SettingButton.tsx
@@ -11,7 +11,7 @@ function SettingButton({ onClick }: Props) {
       onClick={onClick}
     >
       <svg
-        className="w-6 h-6"
+        className="w-7 h-7"
         fill="currentColor"
         viewBox="0 0 20 20"
         xmlns="http://www.w3.org/2000/svg"
